8b48eb8d4da577f7262ecb2a3fb427effd4c794e
Karthik Nandam <carthicknandam@gmail.com>
2025-05-31 18:03:55 +0000

    add run functionality to server

--- services/notification-service/src/server.ts
+++ services/notification-service/src/server.ts
@@ -1,1 +1,15 @@
+import env from "dotenv";
+import express, { Request, Response } from "express";
+import { runConsumer } from "./utils/kafka";
 
+env.config();
+const PORT = process.env.PORT || 3006;
+const server = express();
+
+runConsumer();
+
+server.get("/", (req: Request, res: Response) => {
+  res.json({ success: true, message: "Notifiction servie is running....." });
+});
+
+server.listen();
